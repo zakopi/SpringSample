b36b0e2c224d7da69ae4f1398651f80543134ab7
zakopi <nanayamait@gmail.com>
2021-06-13 00:33:41 +0900

    chap.16 gradleによるSpringBootプロジェクト作成

---
+++ src/main/java/com/example/demo/config/WebMvcControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class WebMvcControllerAdvice {
+
+    /*
+     * This method changes empty character to null
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+}
